1b39424afe7654eb05d13c132bbde893934e651d
Ravi Byakod <rbyakod@gmail.com>
2022-07-25 16:15:38 -0700

    Working on localhost with python-dotenv

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"darekarabhishek@gmail.com\"\n\
+email = \"rbyakod@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-port = $PORT\n\
+port = 8081\n\
 " > ~/.streamlit/config.toml
